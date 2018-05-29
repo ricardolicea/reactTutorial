4afc2628f1c219122147cdade0e71ff0882db012
ricardolicea <ricardo.liceamata@gmail.com>
2018-05-29 14:48:40 -0500

    Commit

--- tv-series-app/src/Components/App/index.js
+++ tv-series-app/src/Components/App/index.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import Intro from '../Intro';
 import './App.css';
+import 'whatwg-fetch'
+import Series from '../../containers/series';
 
 
 
-
-
+/*Clase APP la cual es un componente. Tiene un estado que es un arreglo de series
+y dos funciones
+--componentDidMount()
+--render()
+*/
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    const series = ["Vikings", "Game of Thrones"];
-    setTimeout(() => {
-      this.setState({ series: series});
-
-    }, 2000);
-  }
+  
 
 
 
 
-
+/*La funcion render, es la que "renderea". En este caso renderea unos divs
+y el tamaño del arreglo de series, al cual accesamos con el estado del arreglo
+*/
   render() {
     return (
       <div className="App">
@@ -30,7 +27,7 @@ class App extends Component {
           <h1 className="App-title">Tv series list</h1>
         </header>
         <Intro message = "Here you can find all of your most loved series"/>
-        The length of series array = {this.state.series.length}
+        <Series />
       </div>
     );
   }
